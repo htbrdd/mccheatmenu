503cc8433019a68d180744850e40acde839eb000
htbrdd <htbrdd@gmail.com>
2020-03-06 19:22:15 -0500

    --

--- src/main/java/net/mcreator/guitesting/MCreatorCheatmenustartprocedure.java
+++ src/main/java/net/mcreator/guitesting/MCreatorCheatmenustartprocedure.java
@@ -1,6 +1,13 @@
 package net.mcreator.guitesting;
 
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.advancements.AdvancementProgress;
+import net.minecraft.advancements.Advancement;
+
+import java.util.Iterator;
 
 @Elementsguitesting.ModElement.Tag
 public class MCreatorCheatmenustartprocedure extends Elementsguitesting.ModElement {
@@ -17,6 +24,18 @@ public class MCreatorCheatmenustartprocedure extends Elementsguitesting.ModEleme
 		if (!entity.world.isRemote && entity.world.getServer() != null) {
 			entity.world.getServer().getCommandManager()
 					.handleCommand(entity.getCommandSource().withFeedbackDisabled().withPermissionLevel(4), "cheatmenustart");
+		}
+		if (entity instanceof ServerPlayerEntity) {
+			Advancement _adv = ((MinecraftServer) ((ServerPlayerEntity) entity).server).getAdvancementManager().getAdvancement(
+					new ResourceLocation("guitesting:cheatmenuget"));
+			AdvancementProgress _ap = ((ServerPlayerEntity) entity).getAdvancements().getProgress(_adv);
+			if (!_ap.isDone()) {
+				Iterator _iterator = _ap.getRemaningCriteria().iterator();
+				while (_iterator.hasNext()) {
+					String _criterion = (String) _iterator.next();
+					((ServerPlayerEntity) entity).getAdvancements().grantCriterion(_adv, _criterion);
+				}
+			}
 		}
 	}
 }
